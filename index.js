777f462a2f29ea63dd04ff3580e2084c93206ecb
Jonathan Sharpe <j.r.sharpe+github@gmail.com>
2021-11-15 11:42:40 +0000

    Refactor to position-based implementation

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
 function interpreter(code) {
     const stack = [];
-    let output = "", skipping = false;
+    let output = "", position = 0;
 
-    for (let instruction of code) {
-        if (skipping) {
-            skipping = false;
-            continue;
-        }
+    while (true) {
+        const instruction = code[position];
         if (instruction === "@") {
             break;
         } else if (instruction === "#") {
-            skipping = true;
+            position += 1;
         } else if (instruction === ".") {
             output += `${stack.pop() || 0}`;
         } else if ("0123456789".indexOf(instruction) >= 0) {
             stack.push(parseInt(instruction, 10));
         }
+        position += 1;
     }
     return output;
 }
